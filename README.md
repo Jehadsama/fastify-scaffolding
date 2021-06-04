9294109a8fd0d06c6aa853d1213648fb8bfa1b8b
jehadsama <339364351@qq.com>
2021-06-04 23:25:19 +0800

    feat():

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fastify-scaffolding
-fastify scaffolding
+
+fastify scaffolding is a simple tool to generate project
+
+## feature 01
+
+### 实现脚手架基础功能：模板文件夹建立
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+const { program } = require('commander');
+program
+  .version('0.0.1')
+  .option('-d, --debug', 'output extra degugging', false)
+  .option('-l, --list', 'show commands list');
+
+program.parse(process.argv);
+
+const options = program.opts();
+console.log(options);
